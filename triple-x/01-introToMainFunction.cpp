85a9e8945a4423b2e2fdf5e8258a8fb71d9f4343
Tom Tobar <tomas.tobar@flatironschool.com>
2021-06-25 14:51:27 -0500

    first commit

---
+++ triple-x/01-introToMainFunction.cpp
@@ -0,0 +1,31 @@
+int main()
+{
+    /* 
+    Returning 0 tells the OS that the program has run 
+    successfully. Any other value would signal an error. 
+    */
+
+   /*
+   Running the command `g++ <filename>` wihtin the directory
+   that the file is located in will compile the code that we
+   have written into binary or something that the computer can 
+   understand.  
+   */
+
+    /*
+    To execute the compiled code, type the filename of the compiled 
+    file in the terminal and hit enter.
+    */
+
+   /*
+    INT MAIN() AND WHITE SPACE POINTS
+    ----------------------------------
+
+    1) int main() is required in every c++ program
+    2) Without int main(), a c++ program will not run!
+    3) We return 0 to signal that the program has run successfully
+    4) Compiler ignores white space
+    5) Code style improves readability
+   */
+    return 0;
+}
